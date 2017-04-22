290ebc621bd9c7658807bd782a4e2d5b6581ab30
KamranG12 <kamran.Jabiyev@aplusa-security.com>
2017-04-22 13:35:47 +0400

    headermenuupdate

--- js/topology.js
+++ js/topology.js
@@ -9,10 +9,26 @@ $(document).ready(function() {
     $('.fa-search').click(function(){
     	$('.searchmenuhead').css('display','block');
     	$('.fa-times').css('display','block')
+        $('.fa-search').css('display','none')
     })
 
     $('.fa-times').click(function(){
     	$('.searchmenuhead').css('display','none');
     	$('.fa-times').css('display','none')
+        $('.fa-search').css('display','block')
     })
+
+    $('.pageli').mouseover(function(){
+       $('.page').css('display','block');
+   })
+    $('.page').mouseover(function(){
+       $('.page').css('display','block');
+   })
+
+    $('.pageli').mouseout(function(){
+       $('.page').css('display','none');
+   })
+    $('.page').mouseout(function(){
+       $('.page').css('display','none');
+   })
  }); 
